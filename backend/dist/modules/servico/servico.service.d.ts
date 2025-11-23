0ca788d3930edd11566684168af722defd2f1956
GabrielAkioUrakawa <gabriel.urakawa@gmail.com>
2025-11-23 15:58:00 -0300

    feat: better inserts

--- backend/dist/modules/servico/servico.service.d.ts
+++ backend/dist/modules/servico/servico.service.d.ts
@@ -1,6 +1,7 @@
 import { ServicoRepository } from './servico.repository';
 interface CreateServicoDto {
     servicoCpf: string;
+    funcionarioCpf: string;
     dataHora: string;
     preco: number;
     tipo: string;
